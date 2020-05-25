7380d670008af6028cf522c2842663167b1b5868
humbletim <humbletim@gmail.com>
2020-05-25 19:39:54 -0400

    OpenAL test build

--- indra/newview/llviewerdisplay.cpp
+++ indra/newview/llviewerdisplay.cpp
@@ -86,6 +86,11 @@
 #include "llpresetsmanager.h"
 #include "fsdata.h"
 
+//################################### P373R ######################################
+#include "llviewerVR.cpp"
+llviewerVR gVR;
+//################################### END P373R ##################################
+
 extern LLPointer<LLViewerTexture> gStartTexture;
 extern bool gShiftFrame;
 
@@ -755,6 +760,10 @@ void display(BOOL rebuild, F32 zoom_factor, int subfield, BOOL for_snapshot)
 
 		//upkeep gl name pools
 		LLGLNamePool::upkeepPools();
+		//################################### P373R ######################################
+		sec:
+		gVR.ProcessVRCamera();
+		//################################### END P373R ##################################
 		
 		stop_glerror();
 		display_update_camera();
@@ -1158,7 +1167,29 @@ void display(BOOL rebuild, F32 zoom_factor, int subfield, BOOL for_snapshot)
 		if (!for_snapshot)
 		{
 			render_ui();
-			swap();
+			//################################### P373R ######################################
+			gVR.vrDisplay();
+
+			//################################### END P373R ######################################
+			//swap();
+			//################################### P373R ######################################
+			if (gVR.leftEyeDesc.IsReady  && !gVR.rightEyeDesc.IsReady && gVR.m_fEyeDistance > 0)
+			{
+				goto sec;
+
+
+			}
+			if (!gVR.leftEyeDesc.IsReady  && !gVR.rightEyeDesc.IsReady)
+			{
+				//gVR.HandleInput();
+
+			}
+			if (!gVR.m_bVrActive)
+				swap();
+
+			//################################### END P373R ##################################
+			//swap();
+			
 		}
 
 		
@@ -1587,7 +1618,9 @@ void render_ui_3d()
 	{
 		gUIProgram.bind();
 	}
-
+	//################################### P373R ######################################
+	gVR.RenderControllerAxes();
+	//################################### END P373R ##################################
 	// Coordinate axes
 	// <FS:Ansariel> gSavedSettings replacement
 	//if (gSavedSettings.getBOOL("ShowAxes"))
@@ -1728,7 +1761,9 @@ void render_ui_2d()
 	{
 		gViewerWindow->draw();
 	}
-
+	//################################### P373R ######################################
+	gVR.DrawCursors();
+	//################################### END P373R ##################################
 
 
 	// reset current origin for font rendering, in case of tiling render
@@ -1821,6 +1856,9 @@ void render_disconnected_background()
 
 void display_cleanup()
 {
+	//################################### P373R ######################################
+		gVR.vrStartup(TRUE);
+	//################################### END P373R ######################################
 	gDisconnectedImagep = NULL;
 }
 
