b84b3e680bc95eb7e89a053d224cda571423c531
tim <humbletim@gmail.com>
2024-03-03 18:57:45 -0500

    test

--- build-vc170-64/build.sh
+++ build-vc170-64/build.sh
@@ -29,13 +29,20 @@ perl -pe '
 # disallow direct NSIS invocations
 test -d C:/PROGRA~2/NSIS && mv -v C:/PROGRA~2/NSIS C:/PROGRA~2/NSIS.old
 
-for x in ogg_vorbis openal apr_suite boost expat xxhash zlib-ng jsoncpp xmlrpc-epi glh_linear \
+# for x in `ls autobuild-cache/` ; do echo $x | perl -pe 's@^(\w+)-(.*?)-(windows|common).*$@\1: \2@g' ; done
+
+autobuild print --json | jq '[.installables[]|{key:.name,value:{name,version,copyright,url:(.platforms|.windows64//.common//.windows).archive.url,hash:(.platforms|.windows64//.common//.windows).archive.hash}}]|from_entries' > _autobuild.json
+
+# autobuild print --json | jq '.installables[].platforms|.windows64//.common//.windows|.archive.url' -r > autobuild.urls
+
+packages="ogg_vorbis openal apr_suite boost expat xxhash zlib-ng jsoncpp xmlrpc-epi glh_linear \
          glext libpng nghttp2 curl openssl uriparser tut jpeglib openjpeg meshoptimizer       \
          ndPhysicsStub colladadom minizip-ng pcre libxml2 freetype libhunspell slvoice        \
          dictionaries dullahan vlc-bin cubemaptoequirectangular glod jpegencoderbasic llca    \
-         libndofdev nvapi threejs gntp-growl discord-rpc                                      \
-; do
-  autobuild install $x --verbose
-done
+         libndofdev nvapi threejs gntp-growl discord-rpc"
+
+for x in `echo $packages` ; do
+   jq -r --arg x "$x" '.[$x]|$x+": "+.version+"\n"+.copyright+"\n"' _autobuild.json
+done > build-vc170-64/newview/packages-info.txt
 
 #ninja -C build-vc170-64 -f relative.vc170.env.ninja -j1 llcommon
