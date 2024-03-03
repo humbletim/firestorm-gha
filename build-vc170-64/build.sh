a8baf223facae1038dbac4552194fd5b15979187
tim <humbletim@gmail.com>
2024-03-03 00:34:41 -0500

    test

--- build-vc170-64/build.sh
+++ build-vc170-64/build.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-for x in ogg_vorbis openal apr_suite boost expat xxhash zlib-ng jsoncpp xmlrpc-epi glh_linear glext libpng nghttp2 curl openssl uriparser tut jpeglib openjpeg mes
-hoptimizer ndPhysicsStub colladadom minizip-ng pcre libxml2 freetype libhunspell slvoice dictionaries dullahan vlc-bin cubemaptoequirectangular glod jpegencoderba
-sic llca libndofdev nvapi threejs gntp-growl discord-rpc ; do
+FSVRHASH=$(git -C . describe --always --first-parent --abbrev=7)-$(git -C relative describe --always --first-parent --abbrev=7)
+perl -i -pe "s@FSVRHASH@${FSVRHASH}@g" build-vc170-64/newview/fsversionvalues.h
+cat build-vc170-64/newview/fsversionvalues.h
+
+mkdir -p build-vc170-64/msvc/
+mkdir -p build-vc170-64/CMakeFiles/
+mkdir -p build-vc170-64/copy_win_scripts/
+mkdir -p build-vc170-64/sharedlibs/
+MSYS_NO_PATHCONV=1 cmd.exe /C 'cd build-vc170-64\sharedlibs && mklink /D Release .'
+
+cp -avu indra/newview/icons/development-os/firestorm_icon.ico build-vc170-64/newview/
+cp -avu /c/PROGRA~1/MICROS~2/2022/ENTERP~1/VC/Redist/MSVC/14.38.33135/x64/Microsoft.VC143.CRT/*{140,140_1}.dll build-vc170-64/msvc/
+cp -avu indra/newview/exoflickrkeys.h.in build-vc170-64/newview/exoflickrkeys.h
+cp -avu indra/newview/fsdiscordkey.h.in build-vc170-64/newview/fsdiscordkey.h
+
+perl -pe '
+  s@\$\{VIEWER_VERSION_MAJOR\},\$\{VIEWER_VERSION_MINOR\},\$\{VIEWER_VERSION_PATCH\},\$\{VIEWER_VERSION_REVISION\}@6,6,17,70368@g;
+  s@\$\{VIEWER_VERSION_MAJOR\}\.\$\{VIEWER_VERSION_MINOR\}\.\$\{VIEWER_VERSION_PATCH\}\.\$\{VIEWER_VERSION_REVISION\}@6.6.17.70368@g;
+' indra/newview/res/viewerRes.rc > build-vc170-64/newview/viewerRes.rc
+
+# disallow direct NSIS invocations
+test -d C:/PROGRA~2/NSIS && mv -v C:/PROGRA~2/NSIS C:/PROGRA~2/NSIS.old
+
+for x in ogg_vorbis openal apr_suite boost expat xxhash zlib-ng jsoncpp xmlrpc-epi glh_linear
+         glext libpng nghttp2 curl openssl uriparser tut jpeglib openjpeg meshoptimizer
+         ndPhysicsStub colladadom minizip-ng pcre libxml2 freetype libhunspell slvoice
+         dictionaries dullahan vlc-bin cubemaptoequirectangular glod jpegencoderbasic llca
+         libndofdev nvapi threejs gntp-growl discord-rpc
+; do
   autobuild install $x --verbose
 done
 
-cp -avu /c/PROGRA~1/MICROS~2/2022/ENTERP~1/VC/Redist/MSVC/14.38.33135/x64/Microsoft.VC143.CRT/*{140,140_1}.dll build-vc170-64/msvc/
-
 #ninja -C build-vc170-64 -f relative.vc170.env.ninja -j1 llcommon
-
---
+++ build-vc170-64/nsis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+nsi=build-vc170-64/newview/firestorm_setup_tmp.nsi
+
+grep "openvr_api.dll" $nsi
+  || perl -i.bak  -pe 's@^SetCompressor .*$@SetCompressor zlib@g; s@^(.*?)\b(growl.dll)@$1$2\n$1openvr_api.dll@g' \
+     $nsi
+
+diff $nsi.bak $nsi
+
+test -f C:/PROGRA~2/NSIS.old mv -v C:/PROGRA~2/NSIS.old C:/PROGRA~2/NSIS
+
+# makensis.exe /V2 build-vc170-64/newview/firestorm_setup_tmp.nsi
+
+grep -E ^File "$nsi" | sed -e 's@.*newview[/\\]@@' > "$nsi.txt"
+head -2 "$nsi.txt"
+tail -2 "$nsi.txt"
+
+7z -bb1 -bt -tzip a "$nsi.zip" "@$nsi.txt"
