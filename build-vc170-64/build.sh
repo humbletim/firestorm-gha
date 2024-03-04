3df77d4ce2bf56cc69568f961d8203f3cd482a51
tim <humbletim@gmail.com>
2024-03-03 20:13:32 -0500

    test

--- build-vc170-64/build.sh
+++ build-vc170-64/build.sh
@@ -29,13 +29,25 @@ perl -pe '
 # disallow direct NSIS invocations
 test -d C:/PROGRA~2/NSIS && mv -v C:/PROGRA~2/NSIS C:/PROGRA~2/NSIS.old
 
+test -f /c/hostedtoolcache/windows/Python/3.9.13/x64/Scripts/autobuild.exe && \
+  mv -v /c/hostedtoolcache/windows/Python/3.9.13/x64/Scripts/autobuild.exe /c/hostedtoolcache/windows/Python/3.9.13/x64/Scripts/autobuild.orig.exe
+
 # for x in `ls autobuild-cache/` ; do echo $x | perl -pe 's@^(\w+)-(.*?)-(windows|common).*$@\1: \2@g' ; done
 
-autobuild print --json | jq '[.installables[]|{key:.name,value:{name,version,copyright,url:(.platforms|.windows64//.common//.windows).archive.url,hash:(.platforms|.windows64//.common//.windows).archive.hash}}]|from_entries' > _autobuild.json
+autobuild.orig.exe print --json > autobuild.orig.json
+jq '
+  [.installables[]| . as $root | (.platforms//{}|(.windows64//.common//.windows//{archive:{}}).archive) as $archive | $root | {key:.name,value:{
+     name:.name,
+     copyright: (.copyright // "") | sub("[[::space::]]+$"; ""),
+     version: (.version // ($archive.url | capture(".*/(?<prefix>\\w+)-(?<version>[-.a-zA-Z0-9_]+)-(common|windows)"; "n")//{}).version),
+     url: $archive.url, hash: $archive.hash
+  }}]|from_entries
+' autobuild.orig.json |tee _autobuild.json
 
+#exit
 # autobuild print --json | jq '.installables[].platforms|.windows64//.common//.windows|.archive.url' -r > autobuild.urls
 
-packages="ogg_vorbis openal apr_suite boost expat xxhash zlib-ng jsoncpp xmlrpc-epi glh_linear \
+packages="openvr ogg_vorbis openal apr_suite boost expat xxhash zlib-ng jsoncpp xmlrpc-epi glh_linear \
          glext libpng nghttp2 curl openssl uriparser tut jpeglib openjpeg meshoptimizer       \
          ndPhysicsStub colladadom minizip-ng pcre libxml2 freetype libhunspell slvoice        \
          dictionaries dullahan vlc-bin cubemaptoequirectangular glod jpegencoderbasic llca    \
@@ -46,3 +58,7 @@ for x in `echo $packages` ; do
 done > build-vc170-64/newview/packages-info.txt
 
 #ninja -C build-vc170-64 -f relative.vc170.env.ninja -j1 llcommon
+
+for x in `echo $packages` ; do
+   jq -r --arg x "$x" '.[$x].url' _autobuild.json
+done
--- build-vc170-64/openvr/install.sh
+++ build-vc170-64/openvr/install.sh
@@ -2,8 +2,4 @@
 . $GITHUB_WORKSPACE/build-vc170-64/autobuild.env
 
 set -xe
-autobuild installables add openvr url=file:///$(cygpath -m "$GITHUB_WORKSPACE")/build-vc170-64/openvr/openvr-v1.6.10.8eaf723.tar.bz2 platform=windows64 hash=`md5sum build-vc170-64/openvr/openvr-v1.6.10.8eaf723.tar.bz2|awk '{ print $1 }'` 
-
-
-
-
+autobuild.orig.exe installables add openvr url=file:///$(cygpath -m "$GITHUB_WORKSPACE")/build-vc170-64/openvr/openvr-v1.6.10.8eaf723.tar.bz2 platform=windows64 hash=`md5sum build-vc170-64/openvr/openvr-v1.6.10.8eaf723.tar.bz2|awk '{ print $1 }'`
