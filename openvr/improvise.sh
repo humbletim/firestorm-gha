e32bb0d7b9001a2f11284b9ff34805f6fbb864ba
tim <humbletim@gmail.com>
2024-03-11 04:36:35 -0400

    test

--- openvr/improvise.sh
+++ openvr/improvise.sh
@@ -5,7 +5,6 @@ cd "$(dirname $0)"
 
 #test -f openvr.tar.gz || curl -L -s https://api.github.com/repos/ValveSoftware/openvr/tarball/v1.6.10 -o openvr.tar.gz
 #tar --strip-components=1 -xvf openvr.tar.gz ValveSoftware-openvr-8eaf723/lib/win64 ValveSoftware-openvr-8eaf723/bin/win64 ValveSoftware-openvr-8eaf723/headers ValveSoftware-openvr-8eaf723/LICENSE
-#mkdir -p lib/release include LICENSES
 # cp -av LICENSE LICENSES/openvr.txt
 # cp -av headers/openvr.h include
 # cp -av bin/win64/openvr_api.dll lib/win64/openvr_api.lib lib/release/
@@ -14,7 +13,11 @@ cd "$(dirname $0)"
 # ovr=https://raw.githubusercontent.com/ValveSoftware/openvr/v1.6.10/
 ovr=https://rawcdn.githack.com/ValveSoftware/openvr/v1.6.10/
 tarball=openvr-v1.6.10.8eaf723.tar.bz2
-function _wget() { set -e; wget -nv -nc "$@" ; }
+mkdir -p lib/release include LICENSES
+
+set -x
+
+function _wget() { set -e; echo "fetching $@" >&2 ; wget -nv -nc "$@" ; }
 _wget -O LICENSES/openvr.txt $ovr/LICENSE
 _wget -O include/openvr.h $ovr/headers/openvr.h
 _wget -P lib/release/ $ovr/bin/win64/openvr_api.dll $ovr/lib/win64/openvr_api.lib
