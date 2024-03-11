c72266b9e2d1080551bdef90cbab924a9256de7d
tim <humbletim@gmail.com>
2024-03-10 20:02:11 -0400

    cleanup

--- apply.sh
+++ apply.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-test -d "$build_dir" || { echo "!build_dir" >&2; exit 1; }
 test -d "$source_dir" || { echo "!source_dir" >&2; exit 1; }
-test -d "$root_dir" || { echo "!root_dir" >&2; exit 1; }
 
 here="$(readlink -f $(dirname $0))"
-cd "$here"
-echo "$(jq --sort-keys '. + $p' --argjson p "$(jq '.' meta/packages-info.json)" $build_dir/packages-info.json)" > $build_dir/packages-info.json
-jq '.["p373r-vrmod"]' $build_dir/packages-info.json
 cd $source_dir/newview
 grep P373R llviewerdisplay.cpp >/dev/null || patch -p1 < $here/0001-P373R-6.6.8-baseline-diff.patch
-
-grep p373r_dir $root_dir/build_vars.env || { echo "p373r_dir=$(cygpath -ma $here)" | tee -a $root_dir/build_vars.env ; }
 
