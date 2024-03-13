68d845973626c8349ff2880b6e08d57093603d22
tim <humbletim@gmail.com>
2024-03-13 16:03:30 -0400

    test

---
+++ actions-node-script/action.js
@@ -0,0 +1,65 @@
+// helper to invoke a script in full node20 actions environment
+// -- humbletim 2024.03.13
+ 
+const child_process = require('child_process');
+// process.env.GITHUB_WORKSPACE && process.chdir(process.env.GITHUB_WORKSPACE);
+
+
+var {
+    INPUTS_environment: environment,
+    INPUT_inputs: inputs,
+    INPUTS_run: run,
+    INPUTS_shell: shell,
+    "INPUTS_working-directory": working_directory,
+    GITHUB_WORKSPACE: workspace,
+} = process.env;
+
+console.debug('INPUT_', { inputs, run, shell, environment, workspace, working_directory })
+
+if (!shell || shell == 'bash') {
+    var bash_exe = process.env.PROGRAMFILES ? `${process.env.PROGRAMFILES}\\Git\\usr\\bin\\bash.exe` : 'bash';
+    shell = `${bash_exe} --noprofile --norc -e -o pipefail {0}`
+}
+
+var environment = {};
+var estr= process.env.INPUTS_environment || '';
+(' '+estr).replace(/ ([-_0-9A-Za-z]+)=([^ ]+)/g, (_, k, v) => environment[k]=v);
+
+var args = shell.split(/ +/).filter((x)=> x !== '');
+var idx = args.indexOf('{0}');
+if (!~idx) throw new Error('{0} not found in '+shell);
+args.splice(idx, 1, '-c', run);
+
+var exe = args.shift();
+var options = { stdio: 'inherit', env: environment, shell: false };
+if (working_directory) options.cwd = working_directory;
+else if (workspace) options.cwd = workspace;
+
+console.debug('spawn', exe, args, options)
+
+const bash = child_process.spawn(exe, args, options );
+bash.on('exit', (c)=>{ console.log('exit code', c); process.exit(c); });
+
+//////////////////////////////////////////////////////////////////////////////
+
+// process.stdin.pipe(bash.stdin);
+// bash.stdout.pipe(process.stdout);
+// bash.stderr.pipe(process.stderr);
+
+
+// var reps={};
+// var n=0;
+// function absorb(_, value) {
+//     var key = `__xx__${n++}`;
+//     reps[key]= value;
+//     return ' '+key;
+// }
+// // handle escaped chars
+// shell = ` ${shell} `; 
+// console.log('before', shell)
+// shell = shell.replace(/ "((?:[^"]+|\\")+)"/g, absorb);
+// shell = shell.replace(/ ((?:[^ ]+\\ [^ ]+)+)/g, absorb)
+// var args = shell.split(/ +/)
+//   .map((x)=> x in reps ? reps[x] : x)
+//   .filter((x)=> x !== '');
+// console.log('reps', reps)
