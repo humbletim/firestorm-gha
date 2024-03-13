1ea3ff795da55c20a471468fecdfc4e961b88c49
tim <humbletim@gmail.com>
2024-03-13 19:01:42 -0400

    test

--- actions-node-script/action.js
+++ actions-node-script/action.js
@@ -2,8 +2,6 @@
 // -- humbletim 2024.03.13
  
 const child_process = require('child_process');
-// process.env.GITHUB_WORKSPACE && process.chdir(process.env.GITHUB_WORKSPACE);
-
 
 var {
     INPUT_environment: environment,
@@ -27,22 +25,66 @@ var args = shell
     .split(/ +/).filter(x=>x!=='');
 
 var idx = args.indexOf('{0}');
-if (!~idx) throw new Error('{0} not found in '+shell);
+if (!~idx) throw new Error('{0} not found in shell: '+shell);
 args.splice(idx, 1, '-c', run);
 
-var options = { stdio: 'inherit', env: process.env, shell: false };
-if (working_directory) options.cwd = working_directory;
-else if (workspace) options.cwd = workspace;
+var parsedEnv = (environment||'')
+    .split(/\s*\n\s*/)
+    .filter(x=>x.trim()!=='')
+    .map((x)=>parseKeyValueString(x));
+parsedEnv = Object.assign({}, ...parsedEnv);
 
-var estr= environment || '';
-(' '+estr).replace(/ ([-_0-9A-Za-z]+)=([^ ]+)/g, (_, k, v) => options.env[k]=v);
+var forwardEnv = {};
+(parsedEnv['*']||'').split(/[\s:,;]+/)
+  .forEach((name) => {
+    if (name === '*') Object.assign(forwardEnv, process.env);
+    else if (name in process.env) forwardEnv[name] = process.env[name];
+  });
 
-console.debug('PARSED_', { exe, args, options })
+var options = {
+    stdio: 'inherit',
+    shell: false,
+    cwd: working_directory || workspace || process.env.PWD,
+    env: { ...forwardEnv, ...parsedEnv },
+};
+
+console.debug('PARSED_', { exe, args, options, forwardEnv, parsedEnv })
 
 const bash = child_process.spawn(exe, args, options );
+
 bash.on('exit', (c)=>{ console.log('exit code', c); process.exit(c); });
 
 //////////////////////////////////////////////////////////////////////////////
+
+function parseKeyValueString(input) {
+    const regex = {
+        dquoted: /^(?:^|\s)([^=\s]+)="((?:[^\\"]+|\\["\\])+)"/,
+        squoted: /^(?:^|\s)([^=\s]+)='(.*?)'/,
+        fallback: /^(?:^|\s)([^=\s]+)=((?:[^\\ ]+|\\[ ])+)/,
+    };
+    const result = {};
+
+    str = input;
+    while (str) {
+        const prev = str;
+        var matches = {};
+        for (const [label, re] of Object.entries(regex)) {
+            matches[label]=re.exec(str);
+        }
+        var used = matches.dquoted || matches.squoted || matches.fallback;
+        if (!used) throw new Error(`parseKeyValueString error:\n\t[input] ${input}\n\t[cur] ${str}\n`)
+        var [, key, value] = used;
+        if (used === matches.dquoted) value = value.replace(/\\([\"])/g, '$1');
+        if (used === matches.fallback) value = value.trim().replace(/\\ /g, ' ');
+        result[key] = value;
+        str = str.slice(used[0].length);
+    }
+
+    return result;
+}
+
+// console.log(parseKeyValueString("keyx=\"a value with spaces\" key2=value2 squot='c:\\Program Files' ekey=a\\ value\\ with\\ slashspaces anotherkey=complexvalue=weird other=\"embedded=\\\"super complex\\\"\""))
+// process.exit(1);
 
 // process.stdin.pipe(bash.stdin);
 // bash.stdout.pipe(process.stdout);
