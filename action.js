128f0211672253069bf6b7994f0837cb2d5c1700
tim <humbletim@gmail.com>
2024-03-12 21:25:02 -0400

    test

--- action.js
+++ action.js
@@ -1,2 +1,3 @@
-console.log(process.env)
-
+console.log(process.env.ACTIONS_CACHE_URL)
+require('@actions/core').setOutput('ACTIONS_CACHE_URL', ACTIONS_CACHE_URL)
+require('@actions/core').exportVariable('ACTIONS_CACHE_URL', ACTIONS_CACHE_URL)
