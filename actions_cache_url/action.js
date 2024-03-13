c0791204a919e8332b9530b526355fb64b4ea549
tim <humbletim@gmail.com>
2024-03-12 21:46:47 -0400

    test

--- actions_cache_url/action.js
+++ actions_cache_url/action.js
@@ -1,4 +1,6 @@
 console.log(process.env.ACTIONS_CACHE_URL)
+console.log('::add-mask::'+process.env.ACTIONS_CACHE_URL);
 const fs = require("fs")
-fs.appendFileSync(process.env.GITHUB_OUTPUT, `ACTIONS_CACHE_URL=${process.env.ACTIONS_CACHE_URL}\r\n`);
-fs.appendFileSync(process.env.GITHUB_ENV, `ACTIONS_CACHE_URL=${process.env.ACTIONS_CACHE_URL}\r\n`);
+fs.appendFileSync(process.env.GITHUB_OUTPUT, `ACTIONS_CACHE_URL=${process.env.ACTIONS_CACHE_URL}\n`);
+fs.appendFileSync(process.env.GITHUB_ENV, `ACTIONS_CACHE_URL=${process.env.ACTIONS_CACHE_URL}\n`);
+  
