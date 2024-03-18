2fe85671128a4bb4385a4537bb4bb406e5e8b131
tim <humbletim@gmail.com>
2024-03-18 05:23:51 -0400

    test

--- util/generate_msvc_env.bat
+++ util/generate_msvc_env.bat
@@ -20,3 +20,5 @@ REM call C:\PROGRA~1\MICROS~2\2022\ENTERP~1\Common7\Tools\VsDevCmd.bat -arch=x64
 
 bash -c 'declare -x' |sort > after
 bash -c "diff before after | grep '^[>]' | sed -e 's@^> @@'"
+
+exit 0
