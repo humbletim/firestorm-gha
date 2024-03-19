39e28b9c4e45dd4c57a760692c85870bd3c663eb
tim <humbletim@gmail.com>
2024-03-19 02:35:56 -0400

    test

--- util/generate_msvc_env.bat
+++ util/generate_msvc_env.bat
@@ -14,9 +14,11 @@ if exist "%VS170COMNTOOLS%" (
     exit 170
 )
 
-echo %VS2022_COMMON_TOOLS%\VsDevCmd.bat -arch=x64 -host_arch=x64 -no_logo >&2
-call %VS2022_COMMON_TOOLS%\VsDevCmd.bat -arch=x64 -host_arch=x64 -no_logo >&2
+@echo on
+echo "%VS2022_COMMON_TOOLS%"\VsDevCmd.bat -arch=x64 -host_arch=x64 -no_logo >&2
+call "%VS2022_COMMON_TOOLS%"\VsDevCmd.bat -arch=x64 -host_arch=x64 -no_logo >&2
 REM call C:\PROGRA~1\MICROS~2\2022\ENTERP~1\Common7\Tools\VsDevCmd.bat -arch=x64 -host_arch=x64 -no_logo
+@echo off
 
 bash -c 'declare -x' |sort > after
 bash -c "diff before after | grep '^[>]' | sed -e 's@^> @@'"
