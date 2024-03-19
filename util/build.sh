b8266921c39ca364309d7256b7f0c66ede5702aa
tim <humbletim@gmail.com>
2024-03-19 18:02:56 -0400

    test

--- util/build.sh
+++ util/build.sh
@@ -238,9 +238,10 @@ function 090_ninja_preflight() {( $_dbgopts;
     _assert msvc.env 'test -f $build_dir/msvc.env'
     set -a
     . $build_dir/msvc.env
+    . $BASH_ENV
 
-    bash -c 'echo $msvc_path'
-    bash -c 'which cl.exe > /dev/null' || return 241
+    echo $msvc_path
+    which cl.exe > /dev/null || return 241
     local out="$(ninja -C "$build_dir" -n 2>&1)" || _die_exit_code=$? _die "ninja -n failed\n$out"
     echo "$out" | head -3
     echo "..."
@@ -251,10 +252,9 @@ function 0a0_ninja_build() {( $_dbgopts;
     _assert msvc.env 'test -f $build_dir/msvc.env'
     set -a
     . $build_dir/msvc.env
-    (
-      echo "PATH=$PATH" >&2
-      ninja -C "$build_dir" llpackage
-    ) || _die_exit_code=$? _die "ninja failed"
+    . $BASH_ENV
+    which cl.exe > /dev/null || return 256
+    ninja -C "$build_dir" llpackage || _die_exit_code=$? _die "ninja failed"
 )}
 
 
