e366bb93fd097f2c5a781d1582aebb7174f543a6
tim <humbletim@gmail.com>
2024-03-19 02:32:08 -0400

    test

--- util/generate_msvc_env.bat
+++ util/generate_msvc_env.bat
@@ -4,7 +4,7 @@ bash -c 'declare -x' | sort > before
 
 
 if exist "%VS170COMNTOOLS%" (
-  set VS2022_COMMON_TOOLS="%VS170COMNTOOLS%"
+  set "VS2022_COMMON_TOOLS=%VS170COMNTOOLS%"
 ) else if exist "C:\Program Files\Microsoft Visual Studio\2022\Enterprise" (
   set "VS2022_COMMON_TOOLS=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools"
 ) else if exist "C:\Program Files\Microsoft Visual Studio\2022\Community" (
@@ -14,8 +14,8 @@ if exist "%VS170COMNTOOLS%" (
     exit 170
 )
 
-echo "%VS2022_COMMON_TOOLS%\VsDevCmd.bat" -arch=x64 -host_arch=x64 -no_logo >&2
-call "%VS2022_COMMON_TOOLS%\VsDevCmd.bat" -arch=x64 -host_arch=x64 -no_logo >&2
+echo %VS2022_COMMON_TOOLS%\VsDevCmd.bat -arch=x64 -host_arch=x64 -no_logo >&2
+call %VS2022_COMMON_TOOLS%\VsDevCmd.bat -arch=x64 -host_arch=x64 -no_logo >&2
 REM call C:\PROGRA~1\MICROS~2\2022\ENTERP~1\Common7\Tools\VsDevCmd.bat -arch=x64 -host_arch=x64 -no_logo
 
 bash -c 'declare -x' |sort > after
