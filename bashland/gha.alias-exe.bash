ce75bca2a81b5d35c7842b6adab895edbd588d78
tim <humbletim@gmail.com>
2024-03-22 02:30:47 -0400

    test

--- bashland/gha.alias-exe.bash
+++ bashland/gha.alias-exe.bash
@@ -3,7 +3,7 @@
 function make-stub() {
   CC=${CC:-'/c/Program Files/LLVM/bin/clang'}
   #'-DDEBUG_COMMAND_STRING' 
-  ${CC} '-DBASH="c:/PROGRA~1/Git/usr/bin/bash.exe"' \
+  "${CC}" '-DBASH="c:/PROGRA~1/Git/usr/bin/bash.exe"' \
     fsvr/bashland/BASH_FUNC_invoke.c -o "${1:-bin/BASH_FUNC_invoke.exe}"
 }
 
