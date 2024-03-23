777cfd30ba65bb6c402c6f9ae5dee4c4efe5a582
tim <humbletim@gmail.com>
2024-03-23 14:00:28 -0400

    test

---
+++ util/gha._mktemp.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# note: meant to be sourced inline within a subshell'd function() {( source gha.tmpfiles.bash ; ... )} scope
+
+function gha_mktemp_cleanup_temp_files() {
+    local tmpdir="$1" pid=$2
+    ( test -n "$tmpdir" && test -d "$tmpdir" ) || { echo "!tmpdir=$tmpdir" >&2 ; exit 7 ; }
+    echo "[gha_mktemp_cleanup_temp_files] '$tmpdir/*.$pid.gha_mktemp.*'" >&2
+    for tmpfile in $tmpdir/*.$pid.gha_mktemp.* ; do
+        rm -vf "$tmpfile" >&2
+    done
+}
+_gha_mktemp_pid=${1:-BASHPID}
+_gha_mktemp_pid_tmpdir="$(dirname "$(mktemp -u -p "" --suffix=.$pid.gha_mktemp.XXX)")"
+( test -n "$_gha_mktemp_pid_tmpdir" && test -d "$_gha_mktemp_pid_tmpdir" ) || { echo "!tmpdir=$_gha_mktemp_pid_tmpdir" >&2 ; exit 7 ; }
+trap "gha_mktemp_cleanup_temp_files '$_gha_mktemp_pid_tmpdir' '$_gha_mktemp_pid_tmpdir'" EXIT
+
+function gha-_mktemp() {(
+    set -Euo pipefail
+    local name=$1 pid=$_gha_mktemp_pid
+    ( test -n "$_gha_mktemp_pid_tmpdir" && test -d "$_gha_mktemp_pid_tmpdir" ) || { echo "!tmpdir=$_gha_mktemp_pid_tmpdir" >&2 ; exit 7 ; }
+    local tmpfile="$(mktemp -p "$_gha_mktemp_pid_tmpdir" --suffix=.$pid.gha_mktemp.$1)"
+    echo "$tmpfile"
+)}
