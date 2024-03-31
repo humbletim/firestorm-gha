034b5087ad90bf4e4195b6455c784b6ed4167bce
tim <humbletim@gmail.com>
2024-03-31 16:30:46 -0400

    fix path

--- apply.sh
+++ apply.sh
@@ -2,6 +2,8 @@
 test -d "$source_dir" || { echo "!source_dir" >&2; exit 1; }
 
 here="$(readlink -f $(dirname $0))"
-cd $source_dir/newview
-grep P373R llviewerdisplay.cpp >/dev/null || patch --ignore-whitespace --verbose --merge -p1 < $here/20240331.diff.U.patch
+cd $source_dir/..
+grep P373R llviewerdisplay.cpp >/dev/null || (
+  patch --ignore-whitespace --verbose --merge -p1 < $here/20240331.diff.U.patch
+)
 
