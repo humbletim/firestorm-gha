0d00ff2cceaaef87a8b622e980fc30b7b1563f42
tim <humbletim@gmail.com>
2024-03-31 16:19:19 -0400

    attempt to resolve whitespace merge issues

--- apply.sh
+++ apply.sh
@@ -3,5 +3,5 @@ test -d "$source_dir" || { echo "!source_dir" >&2; exit 1; }
 
 here="$(readlink -f $(dirname $0))"
 cd $source_dir/newview
-grep P373R llviewerdisplay.cpp >/dev/null || patch -p1 < $here/0001-P373R-6.6.8-baseline-diff.patch
+grep P373R llviewerdisplay.cpp >/dev/null || patch --ignore-whitespace --verbose --merge -p1 < $here/20240331.diff.U.patch
 
