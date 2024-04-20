e50efc77b54f5a96fe1b1c2f25ee1fa497781b76
tim <humbletim@gmail.com>
2024-04-20 19:42:29 -0400

    test

--- sgeo-7.1.6/000.bash
+++ sgeo-7.1.6/000.bash
@@ -5,4 +5,28 @@ mkdir -pv repo/p373r
 echo skipping > repo/p373r/applied
 maybe-clone viewer ${hub:-github.com} $repo $ref
 git -C repo/viewer remote add sgeo https://github.com/Sgeo/phoenix-firestorm-alpha
+git fetch sgeo VR_Sgeo_2024
 git -C repo/viewer -c user.email=x -c user.name=y merge --no-commit --no-edit sgeo/VR_Sgeo_2024 || exit 6
+
+cd repo/viewer
+
+patch -p1 < <(cat <<'EOF'
+diff --git a/indra/newview/llviewerVR.h b/indra/newview/llviewerVR.h
+index fd2aa7880..89804262b 100644
+--- a/indra/newview/llviewerVR.h
++++ b/indra/newview/llviewerVR.h
+@@ -1,7 +1,6 @@
+ #pragma once
+
+-#include "../../../openvr/headers/openvr.h"
+-#pragma comment(lib, "../../../openvr/lib/win64/openvr_api.lib")
++#include <openvr.h>
+ #include "llhudtext.h"
+ #include "llgl.h"
+ #include "string.h"
+EOF
+)
+
+git -C repo/viewer diff
+
+true
