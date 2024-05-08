55dec24412c133d3642fdd66a8d144beae8d68e7
tim <humbletim@gmail.com>
2024-05-08 02:33:02 -0400

    pull htxhop patch from crossroads

--- fs-7.1.7-htxhop/000.bash
+++ fs-7.1.7-htxhop/000.bash
@@ -3,14 +3,15 @@
 mkdir -pv repo/p373r
 echo $BASH_SOURCE -- skipping > repo/p373r/applied
 
-gha-cache-restore-fast $cache_id-repo-0000 repo/viewer || (
+gha-cache-restore-fast $cache_id-repo-0001 repo/viewer || (
     set -Euo pipefail
     quiet-clone ${hub:-github.com} $repo $ref repo/viewer
 
     pushd repo/viewer
-      patch -p1 < $nunja_dir/llworldmapmessage.htxhop.patch || exit $?
+      curl https://patch-diff.githubusercontent.com/raw/metaverse-crossroads/phoenix-firestorm/pull/1.patch | patch -p1 || exit $?
+      # patch -p1 < $nunja_dir/llworldmapmessage.htxhop.patch || exit $?
       git diff
     popd
 
-  gha-cache-save-fast $cache_id-repo-0000 repo/viewer  || exit 15
+  gha-cache-save-fast $cache_id-repo-0001 repo/viewer  || exit 15
 )
