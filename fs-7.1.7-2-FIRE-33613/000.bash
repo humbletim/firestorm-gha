918653cecf06e006bf1b879d8017774dc4daeb26
tim <humbletim@gmail.com>
2024-05-16 16:29:19 -0400

    7.1.7-2-FIRE-33613 test

---
+++ fs-7.1.7-2-FIRE-33613/000.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -pv repo/p373r
+echo $BASH_SOURCE -- skipping > repo/p373r/applied
+
+gha-cache-restore-fast $cache_id-repo-0001 repo/viewer || (
+    set -Euo pipefail
+    quiet-clone ${hub:-github.com} $repo $ref repo/viewer
+
+    pushd repo/viewer
+      curl https://patch-diff.githubusercontent.com/raw/metaverse-crossroads/phoenix-firestorm/pull/2.patch | patch -p1 || exit $?
+      git diff
+    popd
+
+  gha-cache-save-fast $cache_id-repo-0001 repo/viewer  || exit 15
+)
