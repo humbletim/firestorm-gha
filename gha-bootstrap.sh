54f3ec6d59667dc8f9fe106317c5aee8dbfe4942
tim <humbletim@gmail.com>
2024-05-19 02:23:46 -0400

    fix

--- gha-bootstrap.sh
+++ gha-bootstrap.sh
@@ -24,6 +24,7 @@ function get_bootstrap_vars() {(
     bd-*) viewer_name=BlackDragon    ;;
     al-*) viewer_name=Alchemy        ;;
   sgeo-*) viewer_name=Sgeo viewer_exe=firestorm ;;
+  test-*) viewer_name=Test viewer_exe=firestorm ;;
        *) viewer_name=Unknown        ;;
   esac
   viewer_id=${viewer_id:-$(echo "$viewer_name" | tr '[:upper:]' '[:lower:]' | sed -e 's@[^-_A-Za-z0-9]@_@g')}
