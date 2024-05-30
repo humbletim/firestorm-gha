54e990800556c696874cb09c2919f5ef421d547c
tim <humbletim@gmail.com>
2024-05-30 15:59:13 -0400

    fix

--- _gha-inline.sh
+++ _gha-inline.sh
@@ -4,8 +4,6 @@ test -v GITHUB_ACTIONS || { echo "GITHUB_ACTIONS expected" >&2 ; exit 3 ; }
 # /d/a/_actions/humbletim/firestorm-gha/tpv-gha-nunja
 export gha_fsvr_dir=$(readlink -f $(dirname "${BASH_SOURCE}"))
 export ghash=$gha_fsvr_dir/gha
-source $ghash/gha.ht-ln.bash
-ht-ln $gha_fsvr_dir/gha-node20-run ./fsvr-action
 
 export PATH="$PATH:/c/ProgramData/Chocolatey/bin"
 jq '.inputs.fstuple|fromjson' $GITHUB_EVENT_PATH \
@@ -14,5 +12,3 @@ jq '.inputs.fstuple|fromjson' $GITHUB_EVENT_PATH \
 
 source $GITHUB_ENV
 export workspace=${GITHUB_WORKSPACE:-${workspace:-$PWD}}
-
-$gha_fsvr_dir/gha-generate-shell-env.bash | tee -a SHELL.env
