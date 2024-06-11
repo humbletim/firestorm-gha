00ea026e6679d017a080f6011c9e23f3a77fb953
tim <humbletim@gmail.com>
2024-06-11 19:18:38 -0400

    fix

--- _gha-inline.sh
+++ _gha-inline.sh
@@ -6,9 +6,18 @@ export gha_fsvr_dir=$(readlink -f $(dirname "${BASH_SOURCE}"))
 export ghash=$gha_fsvr_dir/gha
 
 export PATH="$PATH:/c/ProgramData/Chocolatey/bin"
-jq '.inputs.fstuple|fromjson' $GITHUB_EVENT_PATH \
-  | jq -r 'to_entries[]|select(.value!="")|[.key,.value]|join("=")' \
-  | tee -a $GITHUB_ENV
-
+if [[ -v GITHUB_EVENT_PATH ]]; then
+  jq '.inputs.fstuple|fromjson' $GITHUB_EVENT_PATH \
+    | jq -r 'to_entries[]|select(.value!="")|[.key,.value]|join("=")' \
+    | tee -a $GITHUB_ENV
+else
+  mkdir -pv env.d
+  {
+     echo base=$base
+     echo repo=local
+     echo ref=local
+     echo root_dir=$PWD
+  } | tee -a $GITHUB_ENV env.d/local.env
+fi
 source $GITHUB_ENV
 export workspace=${GITHUB_WORKSPACE:-${workspace:-$PWD}}
