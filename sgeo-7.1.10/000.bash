02a71f3bdd53696125e6787709cd4f2916aefb43
humbletim <humbletim@gmail.com>
2024-10-12 20:31:05 -0400

    sgeo-7.1.10 alternate strategy 2

--- sgeo-7.1.10/000.bash
+++ sgeo-7.1.10/000.bash
@@ -9,10 +9,37 @@ gha-cache-restore $cache_id-repo-0000 repo/viewer || (
     quiet-clone ${hub:-github.com} $repo $ref repo/viewer
 
     pushd repo/viewer
-
+    
+    git remote add sgeo https://github.com/Sgeo/phoenix-firestorm-alpha
+    git fetch sgeo VR_Sgeo_2024
     git -c user.email=CITEST -c user.name=CITEST \
-      am $nunja_dir/VR_Sgeo_2024_Firestor_7.1.10.all-inclusive-working-with-openvrh.patch
+      merge --no-edit sgeo/VR_Sgeo_2024
+    
+    # NOTES:
+    # ... capture wholemeal between manually-patched worktree and an original Firestormx worktree
+    # diff -ur --binary . ../Firestormx/  > ../VR_Sgeo_2024.7.1.10.wholmeal.patch
+    # ... blindly copy conflicting files from manually patched work tree:
+    # tar -C ../Fixed -cf - `git status|grep both|sed -e 's@both modified:@@;'`  | tar xf -
+    patch -p1 < $nunja_dir/VR_Sgeo_2024.7.1.10.mergeconflict-fixes.patch
+    
+    git -c user.email=CITEST -c user.name=CITEST commit -m "VR_Sgeo_2024.7.1.10 locally patched"
 
+patch -p1 < <(cat <<'EOF'
+diff --git a/indra/newview/llviewerVR.h b/indra/newview/llviewerVR.h
+index fd2aa7880..89804262b 100644
+--- a/indra/newview/llviewerVR.h
++++ b/indra/newview/llviewerVR.h
+@@ -1,7 +1,6 @@
+ #pragma once
+
+-#include "../../../openvr/headers/openvr.h"
+-#pragma comment(lib, "../../../openvr/lib/win64/openvr_api.lib")
++#include <openvr.h>
+ #include "llhudtext.h"
+ #include "llgl.h"
+ #include "string.h"
+EOF
+)
   git diff
   # git -C repo/viewer diff
   popd
