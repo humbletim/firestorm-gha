0ba31ca21846cf8a6d4ebacc12c3d97e6e723133
humbletim <humbletim@gmail.com>
2024-12-12 12:19:05 -0500

    snapshot testing

--- blueprints/fs-beta-7.1.11.76751/postbuild.bash
+++ blueprints/fs-beta-7.1.11.76751/postbuild.bash
@@ -2,39 +2,63 @@
 
 source $ghash/gha.upload-artifact.bash
 
-mkdir -pv $build_dir/snapshot
+snapshot_dir=$build_dir/${version_full}-snapshot
+mkdir -pv $snapshot_dir
 
-test -s $build_dir/snapshot/includes.tar.xz || (
-    find indra/ -name \*.inl -o -name \*.h > $build_dir/snapshot/all.includes.txt
+# SNAPSHOT METADATA
+mkdir -pv $snapshot_dir/metadata
+cp -a env.d $snapshot_dir/metadata
+cp -av fstuple.json $snapshot_dir/metadata/
+cp -av $build_dir/packages-info.json $snapshot_dir/metadata/
+cp -av $nunja_dir/viewer_version.txt $snapshot_dir/metadata/
+env | grep INPUT > $snapshot_dir/metadata/INPUT.env
+env | grep -i version=  > $snapshot_dir/metadata/version.env
+
+# SNAPSHOT PACKAGES
+mkdir -pv $snapshot_dir/3p
+cp -a $packages_dir/lib/release $snapshot_dir/3p/lib/
+cp -a $packages_dir/include $snapshot_dir/3p/include/
+rm -rf $snapshot_dir/3p/include/webrtc $snapshot_dir/3p/lib/*webrtc*
+# ln $source_dir $snapshot_dir/source
+
+# SNAPSHOT CORRESPONDING SOURCE
+mkdir -pv $snapshot_dir/source
+(
+    pushd repo/viewer/indra
+    find ~+ -name \*.cpp -o -name \*.inl -o -name \*.h > $snapshot_dir/metadata/all.includes.txt
     time (
-        tar -cvf - -T $build_dir/snapshot/all.includes.txt --show-transformed-names \
-            --transform "s|^${PWD#/}/||" \
-             2>$build_dir/snapshot/includes.tar.rsp \
-        | xz -T0 - -c > $build_dir/snapshot/includes.tar.xz
+        tar -cf - -T $snapshot_dir/metadata/all.includes.txt --show-transformed-names \
+        --transform "s|^${PWD#/}/||" \
+         2>$snapshot_dir/metadata/includes.tar.rsp \
+         | tar -xf - -C$snapshot_dir/source #xz -T0 - -c > $snapshot_dir/includes.tar.xz
     )
+    popd
 )
 
-pushd $build_dir/
-
-test -s snapshot/all.objs.rsp || (
-    find ~+ -name \*.c*.obj | grep -vE '(llwebrtc|media_plugins|slplugin)/' > snapshot/all.objs.rsp
-    echo ~+/llwebrtc/llwebrtc.lib >> snapshot/all.objs.rsp
-    # echo ~+/llwebrtc/llwebrtc.dll >> snapshot/all.objs.rsp
-)
-
-test -s snapshot/objs.tar.xz || (
+# SNAPSHOT EMERGED OBJECT FILES
+mkdir -pv $snapshot_dir/objs
+(
+    pushd $build_dir/
+    (
+        echo ~+/llwebrtc/llwebrtc.lib
+        find ~+ -name \*.c*.obj | grep -vE '(llwebrtc|media_plugins|slplugin)/'
+    ) > $snapshot_dir/metadata/all.objs.rsp
     time (
-        tar -cvf - -T snapshot/all.objs.rsp --show-transformed-names \
+        tar -cf - -T $snapshot_dir/metadata/all.objs.rsp --show-transformed-names \
             --transform "s|^${PWD#/}/||" \
             --transform 's|^([^/]+)/.*[.]dir|\1|x' \
-             2>snapshot/objs.tar.rsp \
-        | xz -T0 - -c > snapshot/objs.tar.xz
+             2>$snapshot_dir/metadata/objs.tar.rsp \
+        | tar -xf - -C$snapshot_dir/objs # xz -T0 - -c > snapshot/objs.tar.xz
     )
+    popd
 )
 
-popd
+cd $build_dir
+7z -mx5 -bd -bt -tzip a ${version_full}-snapshot.zip ${version_full}-snapshot/
+grep gha-patch-upload-artifact /d/a/_actions/actions/upload-artifact/v4/dist/upload/index.js || gha-patch-upload-artifact
+echo zipUploadStream=${version_full}-snapshot.zip gha-upload-artifact-fast ${version_full}-snapshot ${version_full}-snapshot.zip
 
+# UPLOAD SNAPSHOT
 
-cp -av $build_dir/packages-info.json $build_dir/snapshot
-
-cd $build_dir && gha-upload-artifact-fast ${version_full}-snapshot snapshot
+# cd $snapshot_dir
+# gha-upload-artifact-fast ${version_full}-snapshot .
