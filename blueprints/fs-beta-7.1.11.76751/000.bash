91e05bad503185ab2135ec8125e336f6e1a7478a
humbletim <humbletim@gmail.com>
2024-12-14 16:20:44 -0500

    snapshot testing

--- blueprints/fs-beta-7.1.11.76751/000.bash
+++ blueprints/fs-beta-7.1.11.76751/000.bash
@@ -3,6 +3,7 @@ maybe-clone viewer ${hub:-github.com} $repo "$ref"
 maybe-clone p373r github.com ${GITHUB_REPOSITORY} P373R_6.6.8
 echo "snapshot test" -- skipping > repo/p373r/applied
 echo '#include <string>' > $build_dir/dummy.cpp
+ls -lrtha $build_dir
 
     pushd repo/viewer
 patch -p1 < <(cat <<'EOF'
