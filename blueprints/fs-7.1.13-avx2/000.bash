4616c0e3dcd15ce687e33eb4785dfa61d802c2bb
humbletim <humbletim@gmail.com>
2025-07-01 00:34:06 -0400

    reconfigure 7.1.13 avx2

--- blueprints/fs-7.1.13-avx2/000.bash
+++ blueprints/fs-7.1.13-avx2/000.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 maybe-clone viewer ${hub:-github.com} $repo "$ref"
 maybe-clone p373r github.com ${GITHUB_REPOSITORY} P373R_6.6.8
-echo "snapshot test" -- skipping > repo/p373r/applied
+#echo "snapshot test" -- skipping > repo/p373r/applied
 
 echo '#include <string>' | tee build/dummy.cpp
 mkdir -p build/newview
-touch build/newview/firestorm-bin.exe
+#touch build/newview/firestorm-bin.exe
 
 pushd repo/viewer
   patch -p1 < <(cat <<'EOF'
