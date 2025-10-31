0ab7528a72cde6d1d32c88963608d90353f14e21
humbletim <humbletim@gmail.com>
2025-10-30 20:09:47 -0400

    avx2-noavx

--- blueprints/fs-7.2.2-preview-noavx/000.bash
+++ blueprints/fs-7.2.2-preview-noavx/000.bash
@@ -2,15 +2,18 @@
 
 avx2_nunja_dir=${nunja_dir/noavx/avx2}
 
-# clone avx2 blueprint
-/usr/bin/find $avx2_nunja_dir/ -maxdepth 1 -type f ! -name 000.bash -exec cp -av {} $nunja_dir/ \;
+pushd $nunja_dir
+  # clone avx2 blueprint
+  /usr/bin/find $avx2_nunja_dir/ -maxdepth 1 -type f ! -name 000.bash -exec cp -av {} ./ \;
 
-# and de-avx2-ify it
-perl -i'' -pe 's@--avx2=ON@--avx2=OFF@g' *
-perl -i'' -pe 's@ ?(/arch:AVX2|-DUSE_AVX2_OPTIMIZATION)@@g' *
+  # and de-avx2-ify it
+  perl -i'' -pe 's@--avx2=ON@--avx2=OFF@g' *
+  perl -i'' -pe 's@ ?(/arch:AVX2|-DUSE_AVX2_OPTIMIZATION)@@g' *
 
-# confirm no residue
-fgrep -i avx2 * >&2
+  # confirm no residue
+  fgrep -i avx2 * >&2
+
+popd
 
 # source the original 000.bash
 . ${avx2_nunja_dir}/000.bash
