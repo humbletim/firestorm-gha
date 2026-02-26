9783f6053c65f2d6ab4a70a9ad6d9359344f109b
tim <humbletim@gmail.com>
2026-02-26 01:42:51 -0500

    tweaks

--- .github/workflows/ReleaseFS_open.yml.bash
+++ .github/workflows/ReleaseFS_open.yml.bash
@@ -166,7 +166,7 @@ function generate_devtime() {
     ###########################################################################
     echo "SNAPSHOT PACKAGES..." >&2
     mkdir -pv $snapshot_dir/3p/lib
-    for x in `ls -1 $packages_dir/lib/release | grep -v webrtc` ; do
+    for x in `ls -1 $packages_dir/lib/release/{*.lib,*.dll} | grep -v webrtc` ; do
         cpsync $packages_dir/lib/release/$x $snapshot_dir/3p/lib/
     done
     mkdir -pv $snapshot_dir/3p/include
