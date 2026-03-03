3bc3a61a458e9d9afb85821ccb0e86664ac0c05e
tim <humbletim@gmail.com>
2026-03-03 14:57:45 -0500

    include pipeline.cpp .glsl source in devtime

--- .github/workflows/ReleaseFS_open.yml.bash
+++ .github/workflows/ReleaseFS_open.yml.bash
@@ -187,6 +187,9 @@ function generate_devtime() {
     fi
     cp -ua $build_dir/newview/viewerRes.rc $snapshot_dir/source/ || true
 
+    
+    cp --parents app_settings/shaders/class1/deferred/CASF.glsl /dev/shm/fs-open/source 
+
     if true; then #[[ $viewer_bin == secondlife ]] ; then
         cpsync $packages_dir/llphysicsextensions* $snapshot_dir/source || true
     fi
